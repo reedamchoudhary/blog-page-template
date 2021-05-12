8926a10d7b433006ff1beb77471550f684d8b3ef
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-12 17:21:13 +0530

    destrusturing of states

--- dummy-react/dummyreact/src/Destructuring.jsx
+++ dummy-react/dummyreact/src/Destructuring.jsx
@@ -2,9 +2,17 @@ import React , {Component} from 'react';
 import "./index.css";
 
 class Destructuring extends Component{
+    constructor(){
+        super()
+        this.state={
+            name:"Reedam",
+            surname:"Choudhary"
+        }
+    }
     render(){
-        const {name, surname} = this.props
-        return <h1> Hey, {name} {surname}</h1>
+        const {name, surname} = this.state;
+
+        return <h1> Hey, {name} {surname} </h1>
     }
 }
 
