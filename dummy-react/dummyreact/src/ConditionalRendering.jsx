833cc2e29899454a2f8eafa816f79af3c378ec29
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-13 15:34:58 +0530

    understanding conditional Rendering

--- dummy-react/dummyreact/src/ConditionalRendering.jsx
+++ dummy-react/dummyreact/src/ConditionalRendering.jsx
@@ -6,13 +6,34 @@ class ConditionalRendering extends React.Component{
     constructor(){
         super();
         this.state = {
-            inLoggedIn : false
+            inLoggedIn : true
         }
     }
 
     render(){
-        if(this.state.inLoggedIn) return <h1>Hello Reedam</h1>
-        else return <h1>Hi Guest</h1>
+        //1. using if-else:-
+
+        // if(this.state.inLoggedIn) return <h1>Hello Reedam</h1>
+        // else return <h1>Hi Guest</h1>
+
+        //2. using element variable:-
+        // let message;
+        // if(this.state.isLoggedIn) message = "Hello Reedam"
+        // else message = "Hello Guest"
+        // return (
+        //     <h1>{message}</h1>
+        // )
+
+        //3. Using ternary operator:-
+
+        // return(
+        //     this.state.inLoggedIn ?
+        //     <div>Hello Reedam</div> : 
+        //     <div>Hello Guest</div>
+        // )
+
+        //4. short circuit operation :-
+        return this.state.inLoggedIn && <div>Hello reedam</div>
     }
 }
 
